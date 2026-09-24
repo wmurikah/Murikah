4c31f9bada837994a50f1f277ea13b0e501741b7
Wilberforce Murikah <wmurikah@gmail.com>
2026-09-24 11:31:19 +0300

    feat: add Phase 7 competency passport core

---
+++ tutor/railway/virtual_internship/passport/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic Phase 7 Competency Passport domain package."""
+from .definitions import LEVELS, LEVEL_LABELS, LEVEL_SEMANTICS, LEVEL_FRAMEWORK_VERSION
+from .strength import EVIDENCE_STRENGTH_RULESET_VERSION
+from .aggregation import PASSPORT_AGGREGATION_RULESET_VERSION, aggregate_competency
+__all__=["LEVELS","LEVEL_LABELS","LEVEL_SEMANTICS","LEVEL_FRAMEWORK_VERSION","EVIDENCE_STRENGTH_RULESET_VERSION","PASSPORT_AGGREGATION_RULESET_VERSION","aggregate_competency"]
